5c0f219ac1c917375a418a3e6f40631c736afe42
weak1337 <57501119+weak1337@users.noreply.github.com>
2020-03-10 21:47:22 +0100

    Update offsets.h

--- EXP Farmer/offsets.h
+++ EXP Farmer/offsets.h
@@ -2,7 +2,7 @@
 #include <cstdint>
 //01-21-2020
 namespace Offsets {
-	constexpr std::uintptr_t GameManager = 0x52b95c8;
-	constexpr std::uintptr_t RoundManager = 0x52a8e98;
-	constexpr std::uintptr_t GameProfile = 0x52a8e68;
+	constexpr std::uintptr_t GameManager = 0x53B7178;
+	constexpr std::uintptr_t RoundManager = 0x53a6a40;
+	constexpr std::uintptr_t GameProfile = 0x53a42a0;
 }

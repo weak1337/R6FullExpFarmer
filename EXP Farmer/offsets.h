34797f5cfceebce4b56ebe2826c86407c424a499
weak1337 <57501119+weak1337@users.noreply.github.com>
2020-06-29 21:26:06 +0200

    Delete offsets.h

--- EXP Farmer/offsets.h
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-#include <cstdint>
-//01-21-2020
-namespace Offsets {
-	constexpr std::uintptr_t GameManager = 0x53B7178;
-	constexpr std::uintptr_t RoundManager = 0x53a6a40;
-	constexpr std::uintptr_t GameProfile = 0x53a42a0;
-}
